19d25fbfeda4ed7df90d6c9de58a1b463b755495
wahyu arif <wahyuarifkurniawan@outlook.com>
2024-02-27 23:36:00 +0700

    1 day

---
+++ 1-day/dataType.js
@@ -0,0 +1,8 @@
+let name = `wahyu arif kurniawan`;
+let firsName;
+let number = 628562759112;
+let boolean = true;
+let floatPoint = 10.5;
+let empty = null;
+
+console.info(typeof name, typeof number, typeof boolean, typeof floatPoint, typeof firsName, typeof empty);
---
+++ 1-day/variable.js
@@ -0,0 +1,7 @@
+// string, boolean, undefined and null data types
+
+let firstName = `wahyu`;
+let isMan = `true`;
+let nothing;
+let kosong = null;
+console.info(typeof firstName, typeof isMan, typeof nothing, typeof kosong);
